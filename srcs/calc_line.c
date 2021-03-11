61c6089a24effd200d1b721008c091722f622f8d
Malborn Rosie <mrosie@er-e2.kzn.21-school.ru>
2021-03-11 18:49:42 +0300

    sides fix

--- srcs/calc_line.c
+++ srcs/calc_line.c
@@ -6,7 +6,7 @@
 /*   By: mrosie <mrosie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 13:38:13 by mrosie            #+#    #+#             */
-/*   Updated: 2021/03/11 17:16:08 by mrosie           ###   ########.fr       */
+/*   Updated: 2021/03/11 18:39:26 by mrosie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,8 @@ static	void	calc_textr_param(t_drw *drw, t_all *all, t_tex *s)
 {
 	all->tex.curr_x = drw->x;
 	s->textr_x = (int)(drw->wall_x * (float)(s->x_wd));
-	if (((s == &all->tex.nrt || s == &all->tex.sth) && drw->ray_dir_x < 0)
-		|| ((s == &all->tex.est || s == &all->tex.wst) && drw->ray_dir_y > 0))
+	if (((s->side == 1 || s->side == 3) && drw->ray_dir_x < 0)
+		|| ((s->side == 2 || s->side == 0) && drw->ray_dir_y > 0))
 		s->textr_x = s->x_wd - s->textr_x - 1;
 	s->ratio = 1.0 * s->x_wd / all->lin.height;
 	s->start = (all->lin.start - all->map.height_y / 2 +
